94e1297986bb10957037f3ee7e2faf40b3e7d610
zikriyeurkmez@gmail.com <zikriyeurkmez@gmail.com>
2021-05-15 08:11:34 +0300

    add hello changes

--- Hello/HelloWebApi/Controllers/WeatherForecastController.cs
+++ Hello/HelloWebApi/Controllers/WeatherForecastController.cs
@@ -35,5 +35,18 @@ namespace HelloWebApi.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<WeatherForecast> GetById(string id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray()[0];
+        }
     }
 }
