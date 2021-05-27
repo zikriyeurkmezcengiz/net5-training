450d13c3a2b706f00a82e9eceedb9143a85216ac
zikriyeurkmez@gmail.com <zikriyeurkmez@gmail.com>
2021-05-27 08:00:12 +0300

    add create book test class

---
+++ BookStore/Tests/Webapi.UnitTests/Application/BookOperations/Commands/CreateBook/CreateBookCommandTests.cs
@@ -0,0 +1,16 @@
+testsusing System;
+using Xunit;
+
+namespace Webapi.UnitTests.Application.BookOperations.Commands.CreateBook
+{
+    public class CreateBookCommandTests
+    {
+        [Fact]
+        public void WhenAlreadyExistBookIsGiven_AlreadyExistExceptionMessage_ShouldBeReturnes()
+        {
+            //arrange
+            //act
+            //assert
+        }
+    }
+}
